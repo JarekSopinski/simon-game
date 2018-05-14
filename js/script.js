9392782a9cf8dde6a1def062f3703b88868ed3d6
JarekSopinski <jarek.sopinski@gmail.com>
2018-05-14 08:43:08 +0200

    Cached buttons

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const $btnGreen = $("#btnGreen");
+const $btnRed = $("#btnRed");
+const $btnYellow = $("#btnYellow");
+const $btnBlue = $("#btnBlue");
+const $counterValue = $("#counterValue");
+const $btnStart = $("#btnStart");
+const $btnStrict = $("#btnStrict");
+const $strictDiode = $("#strictDiode");
+const $btnOff = $("#btnOff");
+const $btnOn = $("#btnOn");
+
+
